19388be17f26df208d45c71922dfbdb0c7e16c75
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-02-05 17:54:47 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,7 +1,7 @@
-from datavisualization import data_vis
+from data_cleaning import data_preproc
 
 def feat_eng():
-    data = data_vis()
+    data = data_preproc()
     print(data)
     
     data.to_csv("Mutual_Fund_Annual_Turnover.csv", index=False)
